75a7ecc223bec0b5ecb187c40b404c553b6d2482
marconobile <marconobile.007.mn@gmail.com>
2024-11-27 16:21:49 +0100

    add: support for scalars, equivariants and concatenation

--- source/scripts/fingerprint_extractor.py
+++ source/scripts/fingerprint_extractor.py
@@ -1,27 +1,40 @@
 import h5py
 from torch_scatter import scatter
 from geqtrain.data import AtomicDataDict
-
+import torch
 
 class DescriptorWriter(object):
-    def __init__(self, feat_dim:int=32, normalize_wrt_atom_count:bool=False, only_scalars:bool=True):
+    def __init__(self,
+                 feat_dim:int=32,
+                 normalize_wrt_atom_count:bool=False,
+                 only_scalars:bool=True,
+                 cat_local_features:bool=False
+                ):
+
       self.only_scalars = only_scalars
       self.normalize_wrt_atom_count = normalize_wrt_atom_count
       self.feat_dim = feat_dim
-      self.field = AtomicDataDict.NODE_OUTPUT_KEY
+      self.context_aware_interaction_field = AtomicDataDict.NODE_OUTPUT_KEY
+      self.cat_local_features = cat_local_features
+      self.local_interaction = AtomicDataDict.NODE_ATTRS_KEY
+
       self.out_field = AtomicDataDict.GRAPH_OUTPUT_KEY
       self.observations, self.gt = [] , []
 
+    @torch.no_grad()
     def __call__(self, pbar, out, ref_data, **kwargs):
       '''
       the inpt is the data obj modified by the nn
       '''
       if self.only_scalars:
         # self.feat_dim nb feat_dim is latent_dim
-        graph_feature = scatter(out[self.field][...,:self.feat_dim], index = out['batch'], dim=0)
+        graph_feature = scatter(out[self.context_aware_interaction_field][...,:self.feat_dim], index = out['batch'], dim=0)
+        if self.cat_local_features:
+          from_local = scatter(out[self.local_interaction][...,:self.feat_dim], index = out['batch'], dim=0)
+          graph_feature =  torch.cat((from_local, graph_feature), dim=-1)
       else:
         # self.feat_dim + 3* self.feat_dim nb feat_dim is latent_dim
-        graph_feature = scatter(out[self.field], index = out['batch'], dim=0)
+        graph_feature = scatter(out[self.context_aware_interaction_field], index = out['batch'], dim=0)
 
 
       if self.normalize_wrt_atom_count:
