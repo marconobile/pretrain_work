1e373d8354c0a9452bab00a2800aae09803c1074
marconobile <marconobile.007.mn@gmail.com>
2025-01-27 17:33:50 +0100

    remove models, now taking them from geqtrain

--- source/model/__init__.py
+++ source/model/__init__.py
@@ -1,10 +1,5 @@
 from ._dipole import DipoleMoment
-from ._node_model import GlobalModel, NodeModel
-from ._global_node_model import GlobalNodeModel
 
 __all__ = [
     DipoleMoment,
-    GlobalModel,
-		NodeModel,
-    GlobalNodeModel,
 ]
